3b769db8871667407f1bb4b792847b7fbf40a6c9
Titobi <olumoyinjoshua@gmail.com>
2023-12-10 10:39:10 -0800

    a code that passes the pycodestyle check

--- console.py
+++ console.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Define the HBnb Console for inputong prompts 
-"""
+Define the HBnb Console for inputong prompts"""
 
 import cmd
 import re
 
+
 def parse(args):
     pass
-
